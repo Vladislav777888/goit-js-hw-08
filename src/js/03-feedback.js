06257211989277e54d78b3e8c4ab6565b37a63a3
Vladyslav Solomakha <vladislavsolomaha08@gmail.com>
2022-10-31 22:12:16 +0200

    make 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form  input'),
+  textarea: document.querySelector('.feedback-form  textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+populateForm();
+
+/*
+ * - Останавливаем поведение по умолчанию
+ * - Убираем сообщение из хранилища
+ * - Очищаем форму
+ */
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
+
+/*
+ * - Получаем значение поля
+ * - Сохраняем его в хранилище
+ * - Можно добавить throttle
+ */
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+/*
+ * - Получаем значение из хранилища
+ * - Если там что-то было, обновляем DOM
+ */
+function populateForm() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage.message;
+    refs.input.value = savedMessage.email;
+  }
+}
